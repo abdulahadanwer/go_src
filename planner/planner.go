0fcb64c9348ddd0158fe4053131f12e903d56697
abdulahadanwer <abdulahadanwer@outlook.com>
2020-05-18 20:19:06 +1200

    added another line

--- planner/planner.go
+++ planner/planner.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	days := 3
+	fmt.Println("My name is Khan")
 	fmt.Println("Your appointment is in", days, "days.")
 	fmt.Println("with a follow up in", days+dates.DaysInweek, "days")
 }
